3ffe0bb7ea06a0ec3d2e24eb074f3a0594f00b86
Adjective-Object <mhuan13@gmail.com>
2014-07-18 00:44:10 -0400

    added missing files

---
+++ js/limbics.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+	$(".intro").each( function(index, item){
+		$(item).removeClass("intro");
+	});
+
+	bodyclasses();
+
+	$.fx.interval = 1000/60;
+
+});
+
+function bodyclasses(){
+	if ($(window).scrollTop() + $(window).height()/2 > $(".fold:nth-child(2)").offset().top){
+		console.log("i");
+		$("body").addClass("pastrut");
+	}else{
+		$("body").removeClass("pastrut");
+	}
+}
+
+$(window).scroll(bodyclasses);
+bodyclasses();
+
+$("a").click(function(evt){
+	if(this.pathname == window.location.pathname){
+		evt.preventDefault();
+		if( this.hash != ""){
+			x = this.hash;
+			$('html, body').animate({
+				scrollTop: $(x).offset().top
+			}, 500, "swing", function(){
+				window.location.hash = (x =="#rut")? "" : x;
+			});
+		}
+	}
+})
